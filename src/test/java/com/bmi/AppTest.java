9bbc92e1c62a1f8086d775bb06983b5e0e7cb1e6
OlenaMyroshnykova <elenamirespania@gmail.com>
2024-11-20 22:44:01 +0100

    fixed tests

--- src/test/java/com/bmi/AppTest.java
+++ src/test/java/com/bmi/AppTest.java
@@ -12,22 +12,18 @@ public class AppTest {
 
     @Test
     public void testMain() {
-        // Симулируем ввод пользователя: вес = 70 кг, рост = 1.75 м
+       
         String simulatedInput = "70\n1.75\n";
         ByteArrayInputStream inputStream = new ByteArrayInputStream(simulatedInput.getBytes());
         System.setIn(inputStream);
 
-        // Перехватываем стандартный вывод
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
 
-        // Запускаем метод main
         App.main(new String[]{});
 
-        // Получаем вывод программы
         String output = outputStream.toString();
 
-        // Проверяем, что вывод содержит ожидаемые строки
         assertTrue(output.contains("Bienvenido al calculador de IMC"));
         assertTrue(output.contains("Tu índice de masa corporal (IMC) es: 22.86"));
         assertTrue(output.contains("Clasificación: Peso normal"));
